cb6528b523edca044f6ca5201a1a5c8e5900662b
ReshwanthGundeti <89552030+ReshwanthGundeti@users.noreply.github.com>
2022-01-27 12:17:20 -0600

    CalculatorApp

---
+++ SimpleClaculator/SimpleClaculator/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  SimpleClaculator
+//
+//  Created by Gundeti,Reshwanth on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    var operand1:Double = -1.1
+    var operand2:Double = -1.1
+    var calcOperator:Character = " "
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func Button6(_ sender: UIButton) {
+        
+        DisplayLabel.text = DisplayLabel.text! + "6"
+        if operand1 == -1.1
+        {
+            operand1 = 6
+        }
+        else{
+            operand1 = 6
+        }
+    }
+    
+    @IBAction func Button9(_ sender: UIButton) {
+        DisplayLabel.text = DisplayLabel.text! + "9"
+        if operand2 == -1.1
+        {
+            operand2 = 9
+        }
+        else{
+            operand2 = 9
+        }
+        
+        
+    }
+    
+    
+    @IBAction func PlusButton(_ sender: UIButton) {
+        
+        DisplayLabel.text = DisplayLabel.text! + "+"
+        if calcOperator == " "
+        {
+            calcOperator = "+"
+        }
+        
+        
+        
+    }
+    
+    
+    @IBAction func EqualButton(_ sender: UIButton) {
+        
+        DisplayLabel.text = DisplayLabel.text! + "="
+        if(calcOperator == "+")
+        {
+            DisplayLabel.text = DisplayLabel.text! + "\(operand1+operand2)"
+        }
+        
+        
+    }
+    
+    
+    
+}
+
